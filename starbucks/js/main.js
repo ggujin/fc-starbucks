8eac0977714c0d283e6c47010106058ae226a4ab
ggujin <yang97816@gmail.com>
2022-07-28 00:29:37 +0900

    07. 헤더와 드롭다운 메뉴 - 검색

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -5,7 +5,15 @@ searchEl.addEventListener('click', function () { // 익명함수 = 핸들러
   searchInputEl.focus(); // js를 통해 포커스가 가능한 input요소들한테 focus강제 적용 
 });
 
+// 클래스 넣기
 searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused'); // 특정 요소에 class정보를 가지고 있는 객체에서 어떤 클래스 내용을 add 하겠다.
-  searchInputEl.setAttribute('placeholder', '통합검색'); // 지정, html속성(Attribute)을 지정한다.
+  searchInputEl.setAttribute('placeholder', '통합검색'); // 지정, html속성(Attribute)을 지정한다. // input 속성, 문자데이터 추가
+                            //1인수 : 속성이름, 2인수: 속성에들어갈 실제 값.
+});
+
+//포커스 해제, 클래스 빼기
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
 });
